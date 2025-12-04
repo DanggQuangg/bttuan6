6cec1b734050a10699ff916ff84696a6a618b564
DanggQuangg <ThinkPad@DESKTOP-0KPRSEV>
2025-12-04 20:33:19 +0700

    initial commit

---
+++ src/test/java/org/example/bttuan6/Bttuan6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bttuan6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bttuan6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
